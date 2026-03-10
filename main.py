5884ff0ea03b613d81bca8e2cf12fa909c04d05a
Vlitraxes <gabriele.malox@gmail.com>
2026-03-10 23:53:33 +0100

    Added Challenge function command

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import logging
 import os
 
 from dotenv import load_dotenv
+from telegram.error import BadRequest, TelegramError
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
@@ -26,7 +27,7 @@ logging.basicConfig(format=LOGGING_PATTERN, level=logging.INFO)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """function to be activated whenever the "/start" command is sent"""
-    if not update.effective_chat:
+    if not update.effective_chat or not update.effective_user:
         return
 
     username = update.effective_user.first_name
@@ -38,6 +39,36 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             f"Your ID is {userid}, share it with your friends to play togheter"
         )
     )
+
+async def challenge(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Sends an invite to an user with the corresponding ID"""
+    if not update.message or not update.effective_user:
+        return
+
+    if not context.args or not context.args[0].isdigit():
+        await update.message.reply_text("Non-existent or invalid ID code given")
+        return
+
+    sender_id = update.effective_user.id
+    sender_username = update.effective_user.first_name
+
+    if sender_id == int(context.args[0]):
+        await update.message.reply_text("You can't challenge yourself!")
+        return
+
+    challengeduser_id = int(context.args[0])
+    #Here should go the logic that connects the 2 users via database
+    try:
+        await update.message.reply_text("Challenge Sent")
+        await context.bot.send_message(chat_id=challengeduser_id,
+        text=f"{sender_username} (ID = {sender_id}) has sent you an invite!"
+        )
+    except BadRequest:
+        await update.message.reply_text(
+            "Error delivering the challenge. ID code may be invalid or non-existent."
+        )
+    except TelegramError:
+        await update.message.reply_text("An unexpected error occurred while sending the challenge.")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -66,9 +97,11 @@ if __name__ == "__main__":
     start_handler = CommandHandler("start", start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     caps_handler = CommandHandler("caps", caps)
+    challenge_handler = CommandHandler("challenge", challenge)
 
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.add_handler(caps_handler)
+    application.add_handler(challenge_handler)
 
     application.run_polling()
