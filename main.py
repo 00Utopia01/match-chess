0e6223aa638000d8b666c783a437427a2b9b11ac
Vlitraxes <gabriele.malox@gmail.com>
2026-03-10 23:14:47 +0100

    Added welcome message

--- main.py
+++ main.py
@@ -28,8 +28,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """function to be activated whenever the "/start" command is sent"""
     if not update.effective_chat:
         return
+
+    username = update.effective_user.first_name
+    userid = update.effective_user.id
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+        chat_id=update.effective_chat.id, text= (
+            f"Welcome {username} to the Chess bot!\n"
+            f"Your ID is {userid}, share it with your friends to play togheter"
+        )
     )
 
 
