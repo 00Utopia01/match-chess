a6e26cd47f1472b3dd269be026c9b9ac984798d1
00Utopia01 <germenaedoardo@gmail.com>
2026-03-11 11:41:12 +0100

    chore: uncomment test_main.py, reformat

--- tests/test_env.py
+++ tests/test_env.py
@@ -4,15 +4,14 @@ import pytest
 
 from src.env import check_token
 
+tokens = [
+    ("", False),
+    ("test123", False),
+    ("12345678:abcdefghijklmnopqrstuvwxyz01_345-78", True),
+]
 
-@pytest.mark.parametrize(
-    "token, validation",
-    [
-        ("", False),
-        ("test123", False),
-        ("12345678:abcdefghijklmnopqrstuvwxyz01_345-78", True),
-    ],
-)
-def test_check_token(token, validation):
-    """test for check_token()"""
+
+@pytest.mark.parametrize("token, validation", tokens)
+def test_check_token(token: str, validation: bool):
+    """test if a token is in a valid format"""
     assert check_token(token) == validation
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,66 +1,54 @@
 """Test for main.py (obsolete)"""
 
-# from unittest.mock import AsyncMock
-
-# import pytest
-# from pytest_mock import MockerFixture
-
-# from main import caps, echo, start
+from unittest.mock import AsyncMock
+import pytest
+from pytest_mock import MockerFixture
+from main import caps, echo, start
 
 
-# @pytest.mark.asyncio
-# async def test_start(mocker: MockerFixture):
-#     """ "Assert if the callback calls the send_message function"""
-#     # Arrange
-#     update = mocker.Mock()
-#     context = mocker.Mock()
-
-#     update.effective_chat.id = 12345
-#     context.bot.send_message = AsyncMock()
-
-#     # Act
-#     await start(update, context)
-
-#     # Assert
-#     context.bot.send_message.assert_called_once_with(
-#         chat_id=12345, text="I'm a bot, please talk to me!"
-#     )
+@pytest.mark.asyncio
+async def test_start(mocker: MockerFixture):
+    """ "Assert if the callback calls the send_message function"""
+    # Arrange
+    update = mocker.Mock()
+    context = mocker.Mock()
+    update.effective_chat.id = 12345
+    context.bot.send_message = AsyncMock()
+    # Act
+    await start(update, context)
+    # Assert
+    context.bot.send_message.assert_called_once_with(
+        chat_id=12345, text="I'm a bot, please talk to me!"
+    )
 
 
-# @pytest.mark.asyncio
-# async def test_echo(mocker: MockerFixture):
-#     """Assert if the callback calls send_message"""
-#     # Arrange
-#     update = mocker.Mock()
-#     context = mocker.Mock()
-
-#     update.effective_chat.id = 12345
-#     update.message.text = "Hello World"
-#     context.bot.send_message = AsyncMock()
-
-#     # Act
-#     await echo(update, context)
-
-#     # Assert
-#     context.bot.send_message.assert_called_once_with(chat_id=12345, text="Hello World")
+@pytest.mark.asyncio
+async def test_echo(mocker: MockerFixture):
+    """Assert if the callback calls send_message"""
+    # Arrange
+    update = mocker.Mock()
+    context = mocker.Mock()
+    update.effective_chat.id = 12345
+    update.message.text = "Hello World"
+    context.bot.send_message = AsyncMock()
+    # Act
+    await echo(update, context)
+    # Assert
+    context.bot.send_message.assert_called_once_with(chat_id=12345, text="Hello World")
 
 
-# @pytest.mark.asyncio
-# async def test_caps(mocker: MockerFixture):
-#     """ "Assert if the callback calls send_message with the right output"""
-#     # Arrange
-#     update = mocker.Mock()
-#     context = mocker.Mock()
-
-#     update.effective_chat.id = 12345
-
-#     context.args = ["hello", "apple", "machine"]
-#     context.bot.send_message = AsyncMock()
-
-#     # Act
-#     await caps(update, context)
-
-#     # Assert
-#     context.bot.send_message.assert_called_once_with(
-#         chat_id=12345, text="HELLO APPLE MACHINE"
-#     )
+@pytest.mark.asyncio
+async def test_caps(mocker: MockerFixture):
+    """ "Assert if the callback calls send_message with the right output"""
+    # Arrange
+    update = mocker.Mock()
+    context = mocker.Mock()
+    update.effective_chat.id = 12345
+    context.args = ["hello", "apple", "machine"]
+    context.bot.send_message = AsyncMock()
+    # Act
+    await caps(update, context)
+    # Assert
+    context.bot.send_message.assert_called_once_with(
+        chat_id=12345, text="HELLO APPLE MACHINE"
+    )
