3927939e899eb916d70342437d612cf21abeaa40
Vlitraxes <gabriele.malox@gmail.com>
2026-03-16 16:44:03 +0100

    Refactor : cleaned chess_logic.py code

--- src/chess_logic.py
+++ src/chess_logic.py
@@ -1,8 +1,10 @@
 """This module contains the chess logic"""
+
+import io
+
+import cairosvg  # type: ignore
 import chess
 import chess.svg
-import cairosvg
-import io
 
 QUEUE: list[int] = []
 
@@ -23,34 +25,33 @@ def move(board: chess.Board, string: str):
     """Moves a piece along the board"""
     string = string.lower()
     string = string.replace(" ", "")
-    
     try:
-        string = chess.Move.from_uci(string)
-    except:
-        return 1# invalid UCI format or non existent string 
-    
-    if string in board.legal_moves:
-        board.push(move)
-    else:
-        if board.is_check():
-            return 2# Invalid move (player under check)
-        else:
-            return 3# Generic Invalid move
+        parsed_move = chess.Move.from_uci(string)
+    except chess.InvalidMoveError:
+        return 1  # invalid UCI format or non existent string
+    if parsed_move in board.legal_moves:
+        board.push(parsed_move)
+        return 2  # success!
+    if board.is_check():
+        return 3  # Invalid move (player under check)
+
+    return 4  # Generic Invalid move
 
 
 def show_board(board: chess.Board):
     """Function that converts the board to png"""
-    svg_string = chess.svg.board(board,size = 400)
-    png_bytes  = cairosvg.svg2png(bytestring=svg_string.encode('utf-8'))
+    svg_string = chess.svg.board(board, size=400)
+    png_bytes = cairosvg.svg2png(bytestring=svg_string.encode("utf-8"))
 
     img = io.BytesIO(png_bytes)
     img.name = "scacchiera.png"
 
-    return img # Returns board's png ready to be sent via BOT
+    return img  # Returns board's png ready to be sent via BOT
 
 
-def matchmaking(id: int):
-    QUEUE.append(id)
+def matchmaking(user_id: int):
+    """Matchmaking function"""
+    QUEUE.append(user_id)
 
     if len(QUEUE) >= 2:
         create_game(QUEUE[0], QUEUE[1])
