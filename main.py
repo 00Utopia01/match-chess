c1f092825ff931ccdd2b76161489d29b6b9253d2
00Utopia01 <germenaedoardo@gmail.com>
2026-03-18 21:24:41 +0100

    fix: fix eula, custom_match callback logic

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ from command.debug.caps import caps
 from command.debug.echo import echo
 from command.eula import eula
 from command.help import command_list as help_command
-from command.play import play
+from command.play import challenge_user, play
 from command.start import start
 from src import env
-from src.callback import callback_finder
+from src.callback import (
+    handle_accept_match,
+    handle_euela_accept,
+    handle_euela_decline,
+    handle_refuse_match,
+)
 from src.logger import LOGGER as log
 
 if __name__ == "__main__":
@@ -47,20 +52,40 @@ if __name__ == "__main__":
     commands_list_handler = CommandHandler("help", help_command)
     start_handler = CommandHandler("start", start)
     play_handler = CommandHandler("play", play)
+    challenge_handler = CommandHandler("challenge_user", challenge_user)
     eula_handler = CommandHandler("eula", eula)
 
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     caps_handler = CommandHandler("caps", caps)
 
+    accept_eula_handler = CallbackQueryHandler(
+        handle_euela_accept, pattern=r"^usr:accept_eula$"
+    )
+    decline_eula_handler = CallbackQueryHandler(
+        handle_euela_decline, pattern=r"^usr:decline_eula$"
+    )
+    accept_match_handler = CallbackQueryHandler(
+        handle_accept_match, pattern=r"^usr:accept_match_(\w+)$"
+    )
+    refuse_match_handler = CallbackQueryHandler(
+        handle_refuse_match, pattern=r"^usr:refuse_match_(\w+)$"
+    )
+
     application.add_handler(echo_handler)
     application.add_handler(caps_handler)
     application.add_handler(play_handler)
+
+    application.add_handler(challenge_handler)
     application.add_handler(commands_list_handler)
 
     application.add_handler(start_handler)
     application.add_handler(eula_handler)
 
-    application.add_handler(CallbackQueryHandler(callback_finder))
+    # application.add_handler(CallbackQueryHandler(callback_finder))
+    application.add_handler(accept_eula_handler)
+    application.add_handler(decline_eula_handler)
+    application.add_handler(accept_match_handler)
+    application.add_handler(refuse_match_handler)
 
     # Running >--------------------------------
     try:
