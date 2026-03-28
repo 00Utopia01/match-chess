ad5dbbab3e328310644573216251679599e8b32e
extragit130 <paoloini05@gmail.com>
2026-03-28 23:04:38 +0100

    feat(eula.py): total edit of callback + refactor

--- command/eula.py
+++ command/eula.py
@@ -3,21 +3,34 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
 
+from command.optout import optout
+from command.register import register
+from src.logger import LOGGER as log
+
+# Text and Button >--------------------------------------
+
 
 def create_button() -> InlineKeyboardMarkup:
-    """Create button markup"""
-    accept_button = InlineKeyboardButton("I accept", callback_data="usr:accept_eula")
-    refuse_button = InlineKeyboardButton("I decline", callback_data="usr:decline_eula")
+    """Create button markup for /eula"""
+    accept_button = InlineKeyboardButton(
+        "register", callback_data="usr:del_and_start_register"
+    )
+    refuse_button = InlineKeyboardButton(
+        "optout", callback_data="usr:del_and_start_optout"
+    )
 
-    options_layout = [[accept_button, refuse_button]]
+    options_layout = [[refuse_button, accept_button]]
 
     return InlineKeyboardMarkup(options_layout)
 
 
+# Command >------------------------------------------
+
+
 async def eula(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """This function creates an interface and shows the user the EULA paragraph"""
-
     if not update.effective_chat:
+        log.error("No effective_chat in eula()")
         return
 
     button_interface = create_button()
@@ -37,3 +50,47 @@ async def eula(update: Update, context: ContextTypes.DEFAULT_TYPE):
         parse_mode="HTML",
         reply_markup=button_interface,
     )
+
+
+# Callback >-----------------
+
+
+async def del_message_and_optout_callback(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+):
+    """Handle callback of 'optout' button of /eula"""
+    if update.effective_user is None:
+        log.error("No effective_user in del_message_and_optout_callback()")
+        return
+
+    query = update.callback_query
+
+    if query is None or query.message is None:
+        log.error("No query or query.message in del_message_and_optout_callback()")
+        return
+
+    await context.bot.delete_message(
+        chat_id=update.effective_user.id,
+        message_id=query.message.message_id,
+    )
+    await optout(update, context)
+
+
+async def del_message_and_register_callback(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+):
+    """Handle callback of 'register' button of /eula"""
+    if update.effective_user is None:
+        log.error("No effective_user in del_message_and_register_callback()")
+        return
+    query = update.callback_query
+
+    if query is None or query.message is None:
+        log.error("No query or query.message in del_message_and_register_callback()")
+        return
+
+    await context.bot.delete_message(
+        chat_id=update.effective_user.id,
+        message_id=query.message.message_id,
+    )
+    await register(update, context)
