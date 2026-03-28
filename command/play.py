e4567edb0d08b1661b5c53dfd4bef41b3ec5525f
extragit130 <paoloini05@gmail.com>
2026-03-28 17:37:42 +0100

    refactor(play.py): split match_request_msg() + add ID search

--- command/play.py
+++ command/play.py
@@ -7,6 +7,7 @@ from telegram.ext import ContextTypes
 
 from src.chess_logic import matchmaking
 from src.db_manager import DB as db
+from src.logger import LOGGER as log
 
 # Messages >---------------------------------
 
@@ -40,13 +41,54 @@ async def no_user_db_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return
 
 
+def create_button(
+    p1_id: str, mode: tuple[int, bool], p1_firstname: str
+) -> InlineKeyboardMarkup | None:
+    """create button layout for match_request_msg()"""
+
+    if not p1_id.isdigit():
+        log.debug("Invalid P1_ID in create_button()")
+        return None
+    if mode[0] != 1 and mode[0] != 2:
+        log.debug("Invalid Mode in create_button()")
+        return None
+
+    accept_button = InlineKeyboardButton(
+        "I accept", callback_data=f"usr:accept_match_{p1_id}_{mode[0]}_{p1_firstname}"
+    )
+    refuse_button = InlineKeyboardButton(
+        "I refuse", callback_data=f"usr:refuse_match_{p1_id}_{mode[0]}_{p1_firstname}"
+    )
+
+    options_layout = [[accept_button, refuse_button]]
+
+    return InlineKeyboardMarkup(options_layout)
+
+
+def set_text(p1_firstname: str, mode: tuple[int, bool]) -> str:
+    """create text for match_request_msg()"""
+    _text = f"{p1_firstname} invited you to a match"
+
+    if mode[0] != 1 and mode[0] != 2:
+        log.debug("Invalid Mode Code in set_text()")
+        return ""
+
+    if mode[1]:
+        if mode[0] == 1:
+            _text = _text + " where you are black"
+        else:
+            _text = _text + " where you are white"
+
+    return _text
+
+
 # pylint: disable=too-many-arguments
 async def match_request_msg(
     *,
     p1_id: str,
     p2_id: str,
-    mode: int,
-    p1_username: str,
+    mode: tuple[int, bool],
+    p1_firstname: str,
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
 ):
@@ -54,28 +96,19 @@ async def match_request_msg(
     if update.effective_user is None:
         return
 
-    accept_button = InlineKeyboardButton(
-        "I accept", callback_data=f"usr:accept_match_{p1_id}_{mode}_{p1_username}"
-    )
-    refuse_button = InlineKeyboardButton(
-        "I refuse", callback_data=f"usr:refuse_match_{p1_id}_{mode}_{p1_username}"
-    )
+    text = set_text(p1_firstname, mode)
 
-    options_layout = [[accept_button, refuse_button]]
+    if text == "":
+        return
 
-    button_interface = InlineKeyboardMarkup(options_layout)
+    button_interface = create_button(p1_id, mode, p1_firstname)
 
-    _text = f"{p1_username} invited you to a match"
-
-    if mode == 1:
-        _text = _text + " where you are black"
-
-    if mode == 2:
-        _text = _text + " where you are white"
+    if button_interface is None:
+        return
 
     await context.bot.send_message(
         chat_id=p2_id,
-        text=_text,
+        text=text,
         reply_markup=button_interface,
     )
 
@@ -117,19 +150,30 @@ def edit_username(username: str) -> str:
     """edit username args"""
     if username[0] == "@":
         username = username[1:]
+
+    if username.isdigit():
+        temp = db.get_username(username)
+        if isinstance(temp, str):
+            username = temp
+        else:
+            log.debug("No username found in db")
+            return ""
     return username
 
 
-def get_color(mode: str) -> int:
+def get_color(mode: str) -> tuple[int, bool]:
     """from a mode string get the color of p1"""
+    is_forced = False
     match mode:
         case "-w":
             p1_is_white = int(1)
+            is_forced = True
         case "-b":
             p1_is_white = int(0)
+            is_forced = True
         case _:
             p1_is_white = randint(0, 1)
-    return p1_is_white
+    return (p1_is_white, is_forced)
 
 
 def get_mode(args: list[str]) -> str:
@@ -170,12 +214,19 @@ async def challenge_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if context.args is None or update.effective_user is None:
         return
-
     if update.message is None or update.effective_chat is None:
         return
+    if update.effective_user.username is None:
+        return
 
-    p1_username = update.effective_user.full_name
+    p1_firstname = update.effective_user.first_name
+
+    p1_username = update.effective_user.username
     p2_username = edit_username(context.args[0])
+
+    if p2_username == "":
+        log.debug("no username faund with this id")
+        return
 
     p2_id = get_p2_id(p2_username=p2_username, p1_username=p1_username)
 
@@ -186,13 +237,13 @@ async def challenge_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await no_user_db_msg(update, context)
         return
 
-    p1_is_white = int(get_color(get_mode(context.args)))
+    mode = get_color(get_mode(context.args))
 
     await match_request_msg(
         p1_id=str(update.effective_user.id),
         p2_id=p2_id,
-        mode=p1_is_white,
-        p1_username=p1_username,
+        mode=mode,
+        p1_firstname=p1_firstname,
         update=update,
         context=context,
     )
