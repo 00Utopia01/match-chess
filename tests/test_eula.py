aa9c24a80d956d745ae475229266e83c23bdd50c
extragit130 <paoloini05@gmail.com>
2026-03-29 01:31:43 +0100

    test(test_eula.py): add tests for eula.py

--- tests/test_eula.py
+++ tests/test_eula.py
@@ -4,20 +4,28 @@ from unittest.mock import AsyncMock, MagicMock
 
 import pytest
 from pytest_mock import MockerFixture
-from telegram import InlineKeyboardMarkup
 
 from command import eula
 
+# Text and Button >---------------------------------------
+
 
 def test_create_button():
-    """Test to create a button"""
+    """Test to check if create_button() work"""
     result = eula.create_button()
-    assert isinstance(result, InlineKeyboardMarkup) is True
+
+    assert result.inline_keyboard[0][0].text == "optout"
+    assert result.inline_keyboard[0][0].callback_data == "usr:del_and_start_optout"
+    assert result.inline_keyboard[0][1].text == "register"
+    assert result.inline_keyboard[0][1].callback_data == "usr:del_and_start_register"
+
+
+# Command >-----------------------------------------------
 
 
 @pytest.mark.asyncio
 async def test_eula_without_chat():
-    """Test if the message is sent when we havo no effective_chat"""
+    """Test if the message is sent without effective_chat"""
     update = MagicMock()
     update.effective_chat = None
 
@@ -44,3 +52,128 @@ async def test_eula_succes(mocker: MockerFixture):
     context.bot.send_message.assert_called_once_with(
         chat_id=123, text=mocker.ANY, parse_mode=mocker.ANY, reply_markup=markup
     )
+
+
+# Callback >---------------------------------------------
+
+
+############ { test del_message_and_optout_callback() } ####################
+
+
+@pytest.mark.asyncio
+async def test_del_message_and_optout_callback_no_user(mocker: MockerFixture):
+    """Test if the message is sent without effective_chat"""
+    update = MagicMock()
+    update.effective_user = None
+
+    context = AsyncMock()
+    optout_mocker = mocker.patch("command.eula.optout", new_callable=AsyncMock)
+
+    await eula.del_message_and_optout_callback(update, context)
+
+    context.bot.delete_message.assert_not_awaited()
+    optout_mocker.assert_not_awaited()
+
+
+query_input = [
+    [None, None],
+    [MagicMock(), None],
+]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("query, message", query_input)
+async def test_del_message_and_optout_callback_no_quey(query, message, mocker):
+    """Test if the message is sent without query and query.message"""
+    update = MagicMock()
+    update.callback_query = query
+    if update.callback_query is not None:
+        update.callback_query.message = message
+
+    context = AsyncMock()
+    optout_mocker = mocker.patch("command.eula.optout", new_callable=AsyncMock)
+
+    await eula.del_message_and_optout_callback(update, context)
+
+    context.bot.delete_message.assert_not_awaited()
+    optout_mocker.assert_not_awaited()
+
+
+@pytest.mark.asyncio
+async def test_del_message_and_optout_callback_correct(mocker: MockerFixture):
+    """Test if the message is sent with correct input"""
+    update = MagicMock()
+    update.effective_user.id = 1234
+    update.callback_query.message.message_id = 4321
+
+    context = AsyncMock()
+    optout_mocker = mocker.patch("command.eula.optout", new_callable=AsyncMock)
+
+    await eula.del_message_and_optout_callback(update, context)
+
+    context.bot.delete_message.assert_called_once_with(
+        chat_id=1234,
+        message_id=4321,
+    )
+    optout_mocker.assert_awaited_once()
+
+
+############# { test del_message_and_register_callback() } ##################
+
+
+@pytest.mark.asyncio
+async def test_del_message_and_register_callback_no_user(mocker: MockerFixture):
+    """Test if the message is sent without effective_chat"""
+    update = MagicMock()
+    update.effective_user = None
+
+    context = AsyncMock()
+    register_mocker = mocker.patch("command.eula.register", new_callable=AsyncMock)
+
+    await eula.del_message_and_register_callback(update, context)
+
+    context.bot.delete_message.assert_not_awaited()
+    register_mocker.assert_not_awaited()
+
+
+query_input = [
+    [None, None],
+    [MagicMock(), None],
+]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("query, message", query_input)
+async def test_del_message_and_register_callback_no_quey(query, message, mocker):
+    """Test if the message is sent without query and query.message"""
+    update = MagicMock()
+    update.callback_query = query
+    if update.callback_query is not None:
+        update.callback_query.message = message
+
+    context = AsyncMock()
+    register_mocker = mocker.patch("command.eula.register", new_callable=AsyncMock)
+
+    await eula.del_message_and_register_callback(update, context)
+
+    context.bot.delete_message.assert_not_awaited()
+    register_mocker.assert_not_awaited()
+
+
+@pytest.mark.asyncio
+async def test_del_message_and_register_callback_correct(mocker: MockerFixture):
+    """Test if the message is sent with correct input"""
+    update = MagicMock()
+    update.effective_user.id = 1234
+    update.callback_query.message.message_id = 4321
+
+    context = AsyncMock()
+    register_mocker = mocker.patch("command.eula.register", new_callable=AsyncMock)
+
+    await eula.del_message_and_register_callback(update, context)
+
+    context.bot.delete_message.assert_called_once_with(
+        chat_id=1234,
+        message_id=4321,
+    )
+    register_mocker.assert_awaited_once()
